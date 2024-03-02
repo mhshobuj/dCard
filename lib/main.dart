81c1084e76a31374619ef150d77ab7568d75c5e3
mhshobuj <mhshobuj37@gmail.com>
2024-03-02 13:48:06 +0600

    root appbar theme color change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dma_card/res/color.dart';
 import 'package:dma_card/utils/routes/routes.dart';
 import 'package:dma_card/utils/routes/routes_name.dart';
 import 'package:dma_card/view_model/area_list_view_model.dart';
@@ -5,6 +6,7 @@ import 'package:dma_card/view_model/auth_view_model.dart';
 import 'package:dma_card/view_model/home_view_model.dart';
 import 'package:dma_card/view_model/login_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -17,6 +19,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Set the status bar color
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: AppColors.buttonColor, // Set the status bar color here
+        statusBarIconBrightness: Brightness.light, // Set the status bar icon color (light or dark)
+      ),
+    );
+    // Create a MaterialColor based on the buttonColor
+    MaterialColor primarySwatch = MaterialColor(
+      AppColors.buttonColor.value,
+      <int, Color>{
+        50: AppColors.buttonColor.withOpacity(0.1),
+        100: AppColors.buttonColor.withOpacity(0.2),
+        200: AppColors.buttonColor.withOpacity(0.3),
+        300: AppColors.buttonColor.withOpacity(0.4),
+        400: AppColors.buttonColor.withOpacity(0.5),
+        500: AppColors.buttonColor.withOpacity(0.6),
+        600: AppColors.buttonColor.withOpacity(0.7),
+        700: AppColors.buttonColor.withOpacity(0.8),
+        800: AppColors.buttonColor.withOpacity(0.9),
+        900: AppColors.buttonColor,
+      },
+    );
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthViewModel()),
@@ -27,7 +52,10 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: primarySwatch,
+          appBarTheme: const AppBarTheme(
+            color: AppColors.buttonColor, // Set the app bar color here
+          ),
         ),
         initialRoute: RoutesName.splash,
         onGenerateRoute: Routes.generateRoute,
--- lib/res/color.dart
+++ lib/res/color.dart
@@ -5,7 +5,7 @@ class AppColors {
 
   static const Color blackColor = Color(0x00FF0000);
   static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color backgroundColor = Color(0xDD2D53FF);
+  static const Color backgroundColor = Color(0xFFB51E23);
 
   static const Color buttonColor =Color(0xFFB51E23);
 
