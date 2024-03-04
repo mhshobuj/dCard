38ad396a5aa83880a00837ff607afe0c23ca9f5c
DMA_Mehedi <dma.bd2018@gmail.com>
2024-03-04 17:13:11 +0600

    rewards and more screen design done

--- lib/view/more/more_screen.dart
+++ lib/view/more/more_screen.dart
@@ -1,48 +1,112 @@
 import 'package:flutter/material.dart';
-
-import '../../res/color.dart';
 import '../../utils/routes/routes_name.dart';
-import '../../view_model/login_view_model.dart';
+import '../../utils/utils.dart';
 
-class MoreScreen extends StatefulWidget {
+class MoreScreen extends StatelessWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
-  State<MoreScreen> createState() => _MoreScreenState();
-}
-
-class _MoreScreenState extends State<MoreScreen> {
-
-  final TokenViewModel _loginViewModel = TokenViewModel();
-
-  @override
   Widget build(BuildContext context) {
+    final int currentYear = DateTime.now().year;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.backgroundColor,
-        centerTitle: true,
-        title: const Text('More'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 20),
         children: [
-          ElevatedButton(
-            onPressed: () {
-              // Call the logout method here
-              logout();
-            },
-            child: const Text('Logout'),
+          // Profile card
+          const SizedBox(height: 20),
+          Center(
+            child: Card(
+              elevation: 3,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        SizedBox(
+                          height: 100, // Adjust height as needed
+                          width: 100, // Adjust width as needed
+                          child: CircleAvatar(
+                            // Replace 'profileImageUrl' with the actual URL of the profile picture
+                            backgroundImage: NetworkImage('profileImageUrl'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    // Username TextFormField
+                    const Text(
+                      'John Doe',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 10),
+                    // Email TextFormField
+                    const Text(
+                      'johndoe@example.com',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          // Other list items
+          _buildListItem(context, 'FAQ', Icons.help_outline, () {
+            Utils.flushBarErrorMessage("Under Development", context);
+          }),
+          _buildListItem(context, 'Support', Icons.support, () {
+            Utils.flushBarErrorMessage("Under Development", context);
+          }),
+          _buildListItem(context, 'About', Icons.info_outline, () {
+            Utils.flushBarErrorMessage("Under Development", context);
+          }),
+          _buildListItem(context, 'Change Password', Icons.security_outlined, () {
+            Utils.flushBarErrorMessage("Under Development", context);
+          }),
+          _buildListItem(context, 'Logout', Icons.logout, () {
+            _logout(context);
+          }),
+          const SizedBox(height: 20),
+          // Copyright notice
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'Copyright © $currentYear Datasoft Manufacturing & Assembly Inc. Limited. All Rights Reserved.',
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.grey, fontSize: 12),
+            ),
           ),
         ],
       ),
     );
   }
 
-  void logout() {
+
+  Widget _buildListItem(BuildContext context, String title, IconData icon, Function onTap) {
+    return InkWell(
+      onTap: () {
+        onTap();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+        ),
+        child: ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          trailing: const Icon(Icons.arrow_forward_ios), // Add arrow icon
+        ),
+      ),
+    );
+  }
+
+  void _logout(BuildContext context) {
     // Implement your logout logic here
     // For example, you can clear user authentication token, navigate to login screen, etc.
-    _loginViewModel.remove();
+    // Once logged out, navigate to the login screen
     Navigator.pushNamedAndRemoveUntil(context, RoutesName.login, (route) => false);
   }
 }
-
--- lib/view/rewards/rewards_screen.dart
+++ lib/view/rewards/rewards_screen.dart
@@ -3,24 +3,114 @@ import 'package:flutter/material.dart';
 import '../../res/color.dart';
 
 class RewardsScreen extends StatefulWidget {
-  const RewardsScreen({super.key});
+  const RewardsScreen({Key? key}) : super(key: key);
 
   @override
   State<RewardsScreen> createState() => _RewardsScreenState();
 }
 
 class _RewardsScreenState extends State<RewardsScreen> {
+  // Dummy rewards data for testing
+  List<Map<String, dynamic>> rewardsList = [
+    {
+      'imageLogo': 'assets/images/dcard_logo.png',
+      'purpose': 'Purchase at SuperMart',
+      'time': '10:30 AM',
+      'amount': 200.0,
+      'rewardValue': 20.0,
+    },
+    {
+      'imageLogo': 'assets/images/dcard_logo.png',
+      'purpose': 'Online Payment',
+      'time': '02:45 PM',
+      'amount': 150.0,
+      'rewardValue': 15.0,
+    },
+    // Add more dummy data as needed
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.backgroundColor,
-        centerTitle: true,
-        title: const Text('Rewards'),
-      ),
-      body: Column(
+    // Calculate total rewards
+    double totalRewards = rewardsList.fold(
+        0, (previousValue, element) => previousValue + element['rewardValue']);
 
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // Total rewards section
+          const SizedBox(height: 10),
+          Card(
+            elevation: 3,
+            margin: const EdgeInsets.all(16),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  // Rewards logo
+                  Image.asset(
+                    'assets/images/reward.png',
+                    width: 40,
+                    height: 40,
+                  ),
+                  const SizedBox(height: 20),
+                  // Total rewards text
+                  const Text(
+                    'Total Rewards',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  // Total rewards value
+                  Text(
+                    totalRewards.toStringAsFixed(2),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                        color: AppColors.buttonColor
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          // Rewards list
+          Expanded(
+            child: ListView.builder(
+              itemCount: rewardsList.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> rewards = rewardsList[index];
+                return Card(
+                  elevation: 3,
+                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: ListTile(
+                    leading: Image.asset(
+                      rewards['imageLogo'],
+                      width: 60,
+                      height: 60,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(rewards['purpose']),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Time: ${rewards['time']}'),
+                        Text('Amount: \$${rewards['amount'].toStringAsFixed(2)}'),
+                        Text('Reward Value: \$${rewards['rewardValue'].toStringAsFixed(2)}'),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
